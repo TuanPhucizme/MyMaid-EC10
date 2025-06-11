c42083539b4f769b14b9e0eae9a999a0a6503664
tuanphuc <phucnguyenphan93@gmail.com>
2025-06-11 23:38:51 +0700

    cấu hình css

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,8 +8,9 @@ import { Toaster } from 'react-hot-toast';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Create a client
+// Cấu hình query client
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
