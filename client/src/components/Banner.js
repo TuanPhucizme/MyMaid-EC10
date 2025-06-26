20b722443beddf55b7a22b32d944d6ee3fb02217
tuanphuc <phucnguyenphan93@gmail.com>
2025-06-26 11:43:51 +0700

    xóa dấu <>

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -3,15 +3,11 @@ import { Carousel } from "react-bootstrap";
 export default function Banner() {
   return (
     <div className="banner-wrapper my-2">
-      <Carousel indicators={false}>
-        <Carousel.Item>
           <img
             className="d-block w-100 banner-img"
             src="https://www.skyweaver.net/images/media/wallpapers/wallpaper1.jpg"
             alt="Banner"
           />
-        </Carousel.Item>
-      </Carousel>
     </div>
   );
 }
