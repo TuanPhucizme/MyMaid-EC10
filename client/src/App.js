cf8792d582c68265498a366a09c79cd374c2a812
tuanphuc <phucnguyenphan93@gmail.com>
2025-06-26 11:43:35 +0700

    import 1 số đường dẫn đến các trang xung quanh

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
+import "./index.css"; // Assuming you have some global styles
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Services from "./components/Services";
+import PartNer from "./pages/RegisterPartnerPage";
+import AboutUs from "./pages/AboutUs";
+import Blog from "./pages/BlogPage";
+import LogIn from "./pages/LoginPage";
+import Register from "./pages/RegisterPage";
 
 function App() {
   return (
-    <div className="bg-light">
+    <>
       <Header />
-      <Banner />
-      <Services />
-    </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="bg-light">
+              <Banner />
+              <Services />
+            </div>
+          }
+        />
+        <Route path="/partner" element={<PartNer />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
