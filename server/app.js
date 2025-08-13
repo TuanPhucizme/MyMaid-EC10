f84cb1cff820da29699922d99e93f64292a4f965
Vinh Nguyen <94222056+VinkRasengan@users.noreply.github.com>
2025-08-13 15:46:03 +0700

    fix lỗi đọc key

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-require('dotenv').config({ path: path.join(__dirname, '.env') });
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const vnpayRouter = require('./routes/payment');
 const cors = require('cors');
